b003e5024d61cfb784d6ac6f3ffd7d61bf7b9ec9
not-matthias <matthias@codspeed.io>
2025-05-23 11:15:22 +0200

    fix: add return value in exposed api

--- includes/core.h
+++ includes/core.h
@@ -13,12 +13,12 @@ InstrumentHooks *instrument_hooks_init(void);
 void instrument_hooks_deinit(InstrumentHooks *);
 
 bool instrument_hooks_is_instrumented(InstrumentHooks *);
-void instrument_hooks_start_benchmark(InstrumentHooks *);
-void instrument_hooks_stop_benchmark(InstrumentHooks *);
-void instrument_hooks_executed_benchmark(InstrumentHooks *, int32_t pid, const char *uri);
-void instrument_hooks_set_integration(InstrumentHooks *, const char *name, const char *version);
+int8_t instrument_hooks_start_benchmark(InstrumentHooks *);
+int8_t instrument_hooks_stop_benchmark(InstrumentHooks *);
+int8_t instrument_hooks_executed_benchmark(InstrumentHooks *, int32_t pid, const char *uri);
+int8_t instrument_hooks_set_integration(InstrumentHooks *, const char *name, const char *version);
 
-void callgrind_start_instrumentation();
-void callgrind_stop_instrumentation();
+int8_t callgrind_start_instrumentation();
+int8_t callgrind_stop_instrumentation();
 
 #endif
